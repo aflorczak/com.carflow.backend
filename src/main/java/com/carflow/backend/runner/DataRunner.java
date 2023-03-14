9ade5753486397770e0f026f3d79c361f362623c
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-03-14 11:30:16 +0100

    Change in DataRunner

--- src/main/java/com/carflow/backend/runner/DataRunner.java
+++ src/main/java/com/carflow/backend/runner/DataRunner.java
@@ -1,5 +1,7 @@
 package com.carflow.backend.runner;
 
+import com.carflow.backend.infrastructure.boundaries.out.cars.entity.CarEntity;
+import com.carflow.backend.infrastructure.boundaries.out.cars.repository.CarRepository;
 import com.carflow.backend.infrastructure.boundaries.out.order.repository.OrderRepository;
 import com.carflow.backend.infrastructure.boundaries.out.order.entity.OrderEntity;
 import org.springframework.boot.ApplicationArguments;
@@ -9,15 +11,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class DataRunner implements ApplicationRunner {
 
-    private final OrderRepository repo;
+    private final OrderRepository orderRepository;
+    private final CarRepository carRepository;
 
-    public DataRunner(OrderRepository repo) {
-        this.repo = repo;
+    public DataRunner(OrderRepository orderRepository, CarRepository carRepository) {
+        this.orderRepository = orderRepository;
+        this.carRepository = carRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Runner");
         OrderEntity orderEntity = new OrderEntity(
+                1,
                 "ACCEPTED",
                 "Stefan Wymyslony",
                 "PZU",
@@ -37,6 +43,28 @@ public class DataRunner implements ApplicationRunner {
                 false,
                 ""
         );
-        repo.save(orderEntity);
+
+        CarEntity carEntity = new CarEntity(
+                1,
+                false,
+                "wroclaw-lotnisko",
+                "Opel",
+                "Astra",
+                524,
+                "DIESEL",
+                5,
+                5,
+                "COMBI",
+                "C",
+                "VIN",
+                "DLK 524854",
+                "15-05-2023",
+                "15-05-2023",
+                null,
+                null
+        );
+
+        carRepository.save(carEntity);
+        orderRepository.save(orderEntity);
     }
 }
