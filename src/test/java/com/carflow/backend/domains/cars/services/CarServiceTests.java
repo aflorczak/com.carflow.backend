f0315141318cc2d3e07145de909b9ecfff92c406
Adrian Florczak <af_site@outlook.com>
2023-04-02 23:22:49 +0200

    Pr Workflow

--- src/test/java/com/carflow/backend/domains/cars/services/CarServiceTests.java
+++ src/test/java/com/carflow/backend/domains/cars/services/CarServiceTests.java
@@ -1,13 +1,10 @@
-//package com.carflow.backend.domains.cars.services;
-//
-//import com.carflow.backend.module.carModule.domain.service.CarService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class CarServiceTests {
-//    @Test
-//    void responseWithNewCar(CarService service) {
-//
-//    }
-//}
+package com.carflow.backend.domains.cars.services;
+
+import org.junit.jupiter.api.Test;
+
+public class CarServiceTests {
+    @Test
+    void dummyTest() {
+
+    }
+}
