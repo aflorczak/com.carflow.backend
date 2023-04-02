a9f2769e63fc8097b190cd6b688eaebb7ba3bef6
Adrian Florczak <af_site@outlook.com>
2023-04-02 17:11:29 +0200

    AttachmentRestApiTest

---
+++ src/test/java/com/carflow/backend/module/attachmentModule/infrastructure/boundary/in/WebApi/v_0_0_1/controller/AttachmentRestApiTest.java
@@ -0,0 +1,9 @@
+package com.carflow.backend.module.attachmentModule.infrastructure.boundary.in.WebApi.v_0_0_1.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttachmentRestApiTest {
+
+}
